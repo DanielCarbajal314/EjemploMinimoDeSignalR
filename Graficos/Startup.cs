cc3b16f194cd75953930799b8cb01c0660d08d6a
DanielCarbajal314 <daniel.carbajal@pucp.edu.pe>
2018-01-28 13:48:27 -0500

    Graficos

---
+++ Graficos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Graficos.Startup))]
+namespace Graficos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ PruebaDeSignalR/Controllers/EnvioDeMensajesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.SignalR;
+using PruebaDeSignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PruebaDeSignalR.Controllers
+{
+    public class EnvioDeMensajesController : ApiController
+    {
+
+        public void Post(Mensaje Mensaje)
+        {
+            var hubDeMensajes = GlobalHost.ConnectionManager.GetHubContext<MensajesHub>();
+            hubDeMensajes.Clients.All.RecibiMensaje(Mensaje);
+        }
+
+        public class Mensaje{
+            public int Id { get; set; }
+            public string Contenido { get; set; }
+            public string Persona { get; set; }
+        }
+    }
+}
